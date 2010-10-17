048b860c52f56cdd17fbd1e85676506611c5c033
Robson Rodrigues <robsonrod@gmail.com>
2010-10-16 21:02:42 -0300

    Leitor de dados com codigo melhorado, sem fazer o console travar

--- ler_dados_serial.rb
+++ ler_dados_serial.rb
@@ -1,9 +1,20 @@
 require "rubygems"
 require "serialport"
 
+#Todos os parametros possiveis para a configuração
+#Por padrão o serialport os usa, caso mude use a seguinte codificação
+#port_str = "/dev/ttyUSB0"
+#baud_rate = 9600
+#data_bits = 8
+#stop_bits = 1
+#parity = SerialPort::NONE
+#sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
 sp = SerialPort.new "/dev/ttyUSB0"
 
-for i in 1..10
-  puts sp.read
+while true do
+  printf(sp.read)
 end
+
+sp.close
 
